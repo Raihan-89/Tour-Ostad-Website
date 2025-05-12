4a8270aeee897b95f2557e3cecb8def64de0df7d
Raihan <iamraihan89@gmail.com>
2025-05-12 23:28:42 +0600

    Add Tour Ostad Project

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// const navMenu = document.getElementById("nav-menu");
+//   const navToggle = document.getElementById("nav-toggle");
+//   const navClose = document.getElementById("nav-close");
+
+// navToggle.addEventListener("click", () => {
+//     navMenu.classList.add("show");
+//     navToggle.style.display = "none";
+// });
+
+// navClose.addEventListener("click", () => {
+//     navMenu.classList.remove("show");
+//     navToggle.style.display = "block";
+// });
+
+
+// window.addEventListener('resize', function () {
+//   const navMenu = document.getElementById('nav-menu');
+//   const navToggle = document.getElementById('nav-toggle');
+//   const navClose = document.getElementById('nav-close');
+
+//   // Remove mobile menu if screen width is more than 720px
+//   if (window.innerWidth > 720) {
+//     navMenu.classList.remove('show');
+//     navToggle.style.display = 'none'; // Hide menu icon on desktop
+//     navClose.style.display = 'none';  // Hide close icon on desktop
+//   } else {
+//     navToggle.style.display = 'block'; // Show toggle on small screens
+//   }
+// });
